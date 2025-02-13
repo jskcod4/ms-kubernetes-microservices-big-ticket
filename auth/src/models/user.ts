6af26fae31f201d21829250647afc4a2642d248d
Jose Angel Rodriguez <jskcod4@gmail.com>
2025-02-12 20:05:47 -0400

    add User model with Mongoose schema and build method

---
+++ auth/src/models/user.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describe the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>("User", userSchema);
+
+export { User };
