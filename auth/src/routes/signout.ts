a2689f951683b2bf24320e870a226e9edccf9027
Jose Angel Rodriguez <jskcod4@gmail.com>
2025-02-18 10:43:58 -0400

    implement signout route to clear session and return empty response

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 
 router.get("/api/users/signout", (req, res) => {
-  res.send("Hi there! /api/users/signout");
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
