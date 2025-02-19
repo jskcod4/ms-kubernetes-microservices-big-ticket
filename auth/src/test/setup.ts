15344c4406944f2a9704b3fd74a70995d28fbbd8
Jose Angel Rodriguez <jskcod4@gmail.com>
2025-02-19 14:32:26 -0400

    refactor current user test to use global signin helper for cookie management

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,7 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
+import request from "supertest";
 import mongoose from "mongoose";
+import { app } from "../app";
 
 let mongo: any;
 beforeAll(async () => {
@@ -25,3 +27,24 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "123456";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie || [];
+};
