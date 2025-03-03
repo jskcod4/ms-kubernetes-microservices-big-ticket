44b229179d7a208e594776a1ad87eb6e3aa6315a
Jose Angel Rodriguez <jskcod4@gmail.com>
2025-03-03 16:39:54 -0400

    refactor: simplify current user route by removing requireAuth middleware and enhance server-side data fetching in index page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,11 +2,31 @@ import axios from "axios";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 export const getServerSideProps = (async () => {
-  const response = await axios.get(
-    "https://ticketing.dev/api/users/currentuser"
-  );
-  return response.data;
-}) satisfies GetServerSideProps<{ currentUser: unknown }>;
+  if (typeof window === "undefined") {
+    // SERVICE_NAME.NAMESPACE.svc.cluster.local
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+
+    return {
+      props: {
+        currentUser: data,
+      },
+    };
+  } else {
+    const { data } = await axios.get("/api/users/currentuser");
+    return {
+      props: {
+        currentUser: data,
+      },
+    };
+  }
+}) satisfies GetServerSideProps<any>;
 
 export default function LoadingPage({
   currentUser,
