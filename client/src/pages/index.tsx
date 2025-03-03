abcc89a48aba134de52542e2df75c3c6c7d65868
Jose Angel Rodriguez <jskcod4@gmail.com>
2025-03-03 17:34:23 -0400

    feat: add sign-in page with form handling and API request integration

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,19 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 import buildClient from "../../api/build-client";
+import { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+} & InferGetServerSidePropsType<typeof getServerSideProps>;
 
 export const getServerSideProps = (async (context) => {
   const { data } = await buildClient(context).get("/api/users/currentuser");
@@ -11,10 +25,12 @@ export const getServerSideProps = (async (context) => {
   };
 }) satisfies GetServerSideProps<any>;
 
-export default function LoadingPage({
-  currentUser,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(currentUser);
+export default function LoadingPage({ currentUser }: AppPropsWithLayout) {
+  const renderedPage = currentUser ? (
+    <h1>Your are signed in</h1>
+  ) : (
+    <h1>Test of project</h1>
+  );
 
-  return currentUser ? <h1>Your are signed in</h1> : <h1>Test of project</h1>;
+  return renderedPage;
 }
